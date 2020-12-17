869d97ca97890ceac0701e962d5b6565f8e6178e
Morgado <morgadodesarrollador@gmail.com>
2020-12-17 22:41:24 +0100

    correcion examen

--- proyectoEmpresa/domenicaSweb/start.sh
+++ proyectoEmpresa/domenicaSweb/start.sh
@@ -4,12 +4,12 @@ set -e
 cp /root/addhosts >> /etc/hosts
 #cat  /root/addhosts >> /etc/hosts
 
-
+#No usas las variables de ENTORNO
 useradd -rm -d /home/domenica -s /bin/bash "domenica"
 #useradd -rm -d /home/"${USUARIO}" -s /bin/bash "${USUARIO}" 
 
 echo "domenica:1234" | chpasswd
-#echo "${USUARIO}:${PASSWD}" | chpasswd
+#echo "${USUARIO}:${PASSWD}" | chpasswd 
 
 cp /root/id_rsa.pub /home/domenica/.ssh/authorized_keys
 #cat /root/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
